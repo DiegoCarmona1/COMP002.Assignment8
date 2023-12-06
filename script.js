3d9d4f08a19f3c494815b9e5ed8c1b3411c9e69c
Diego Carmona <diegocarmona@my.whccd.edu>
2023-12-06 00:22:36 -0800

    added console.log statements

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("Studying the book was really slow for me, whether due to being late night study sessions or due to my lack of understanding, I found most of my understanding and mastery coming from other programmers lessons and answers that I could use to decode how to make functions, methods, values, and bindings.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("Most of my lack of understanding could have been resolved if I had shown up to the class and actively participated alongside the instructor who could have cleared up my confusion and helped me familiarized myself easier, as such any struggles I had are due of my own.");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I had no expectation coming into this course, as such everything I learned was new, refreshing, and albeit frustrating. Exploring a whole new world is far more complex than one could realize.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("All concepts I encountered were new, I rarely found anything I had prior experience with. Although, I can confidently say that along the path to my mastery I found the details of javascript functions to become more vivid in the online actions and sites I would visit regularly. I hope to continue improving.");
